d4dc341a56d0d9a78785fd37c07d120b604d16df
Timon <timon.evenblij@ugent.be>
2016-02-23 23:00:01 +0100

    defined some initial (possibly stub)  classes to start working on a waypoint algorithm

--- frontend/core/communication.h
+++ frontend/core/communication.h
@@ -1,12 +1,13 @@
 // The communication component header
 #include <iostream>
 
+/*!
+This is an example of a comment that doxygen will export. The Communication class...
+
+*/
 class Communication {
   public:
     std::string send_data ();
-    /*!
-    This is an example of a comment that doxygen will export
 
-    */
 
 };
--- frontend/core/core.pri
+++ frontend/core/core.pri
@@ -1,6 +1,10 @@
 INCLUDEPATH += "core"
 DEPENDPATH += "core"
 
-SOURCES += $$PWD/communication.cpp
-HEADERS += $$PWD/communication.h
+SOURCES += $$PWD/communication.cpp \
+    $$PWD/waypointplanning.cpp \
+    $$PWD/drone.cpp
+HEADERS += $$PWD/communication.h \
+    $$PWD/waypointplanning.h \
+    $$PWD/drone.h
 
