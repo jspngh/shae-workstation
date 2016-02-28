d9c9dfcc0958d7945cec337babb2146250073eeb
Timon <timon.evenblij@ugent.be>
2016-02-28 18:53:51 +0100

    added another unit test for mulitiple drone planning

--- tests/frontend_test.cpp
+++ tests/frontend_test.cpp
@@ -23,6 +23,7 @@ private Q_SLOTS:
     void testSimplePathAlgorithm2();
     void testSimplePathAlgorithm3();
     void testSimplePathAlgorithmWithMultipleDrones();
+    void testSimplePathAlgorithmWithMultipleDrones2();
 };
 
 Frontend_Test::Frontend_Test()
@@ -208,7 +209,7 @@ void Frontend_Test::testSimplePathAlgorithmWithMultipleDrones()
     drones.push_back(Drone(2.0));
 
     QGeoRectangle area = QGeoRectangle(QGeoCoordinate(8.0,0.0),QGeoCoordinate(0.0,8.0));
-    SimplePathAlgorithm algorithm = SimplePathAlgorithm(QGeoCoordinate(-1.0,-1.0));
+    SimplePathAlgorithm algorithm = SimplePathAlgorithm(QGeoCoordinate(1.0,1.0));
     algorithm.setWaypointsForDrones(area, &drones);
     std::list<QGeoCoordinate> testList1 = std::list<QGeoCoordinate>();
     std::list<QGeoCoordinate> testList2 = std::list<QGeoCoordinate>();
@@ -266,6 +267,72 @@ void Frontend_Test::testSimplePathAlgorithmWithMultipleDrones()
 
 }
 
+void Frontend_Test::testSimplePathAlgorithmWithMultipleDrones2()
+{
+    std::list<Drone> drones = std::list<Drone>();
+    drones.push_back(Drone(1.5));
+    drones.push_back(Drone(2.0));
+
+    QGeoRectangle area = QGeoRectangle(QGeoCoordinate(8.0,0.0),QGeoCoordinate(0.0,8.0));
+    SimplePathAlgorithm algorithm = SimplePathAlgorithm(QGeoCoordinate(5.0,5.0));
+    algorithm.setWaypointsForDrones(area, &drones);
+    std::list<QGeoCoordinate> testList1 = std::list<QGeoCoordinate>();
+    std::list<QGeoCoordinate> testList2 = std::list<QGeoCoordinate>();
+
+    testList1.push_back(QGeoCoordinate(8.0,4.0));
+    testList1.push_back(QGeoCoordinate(0.0,4.0));
+    testList1.push_back(QGeoCoordinate(0.0,2.5));
+    testList1.push_back(QGeoCoordinate(8.0,2.5));
+    testList1.push_back(QGeoCoordinate(8.0,1.0));
+    testList1.push_back(QGeoCoordinate(0.0,1.0));
+    testList1.push_back(QGeoCoordinate(0.0,-0.5));
+    testList1.push_back(QGeoCoordinate(8.0,-0.5));
+
+    testList2.push_back(QGeoCoordinate(8.0,4.0));
+    testList2.push_back(QGeoCoordinate(0.0,4.0));
+    testList2.push_back(QGeoCoordinate(0.0,6.0));
+    testList2.push_back(QGeoCoordinate(8.0,6.0));
+    testList2.push_back(QGeoCoordinate(8.0,8.0));
+    testList2.push_back(QGeoCoordinate(0.0,8.0));
+
+
+
+    //drones.front().waypoints == testList1
+    double epsilon = 0.000001;
+    int listSize = testList1.size();
+    for(int i=0; i<listSize; i++){
+        QGeoCoordinate calculated = drones.front().waypoints.front();
+        QGeoCoordinate test = testList1.front();
+
+        drones.front().waypoints.pop_front();
+        testList1.pop_front();
+
+        //Compare
+        QVERIFY(abs(calculated.latitude()- test.latitude())<epsilon);
+        QVERIFY(abs(calculated.longitude()- test.longitude())<epsilon);
+
+
+    }
+
+    //drones.back().waypoints == testList2
+    listSize = testList2.size();
+    for(int i=0; i<listSize; i++){
+        QGeoCoordinate calculated = drones.back().waypoints.front();
+        QGeoCoordinate test = testList2.front();
+
+        drones.back().waypoints.pop_front();
+        testList2.pop_front();
+
+        //Compare
+        QVERIFY(abs(calculated.latitude()- test.latitude())<epsilon);
+        QVERIFY(abs(calculated.longitude()- test.longitude())<epsilon);
+
+
+    }
+
+
+
+}
 
 
 QTEST_MAIN(Frontend_Test)
