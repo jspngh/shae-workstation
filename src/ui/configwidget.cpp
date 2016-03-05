b705854b46fa5127982dde1c4b6193922065ef43
vpolflie <vincent.polfliet@ugent.be>
2016-03-06 00:21:28 +0100

    config files extra reading

--- src/ui/configwidget.cpp
+++ src/ui/configwidget.cpp
@@ -80,16 +80,34 @@ void ConfigWidget::initConfScreen(QString f){
        {
           QString line = in.readLine();
           QStringList splitLine = line.split(" ");
-          for(int i = 0; i < splitLine.size(); i++){
-              if(splitLine.at(i) == "Precision:")
-              {
-                  ui->PrecisionValueLabel->setText(splitLine.at(i+1));
-                  ui->precisionSlider->setValue((((QString)splitLine.at(i+1).split("%").at(0)).toInt()));
-                  break;
-              }
-          }
+          if(splitLine.at(0) == "Precision:"){
+                    ui->PrecisionValueLabel->setText(splitLine.at(1));
+                    ui->precisionSlider->setValue((((QString)splitLine.at(0).split("%").at(0)).toInt()));
+            } else if(splitLine.at(0) == "Search"){
+                    ui->searchStrategyComboBox->setCurrentText((QString) splitLine.at(2));
+            }else if(splitLine.at(0) == "Location"){
+                    ui->locateField->setText((QString) splitLine.at(3));
+            }else if (splitLine.at(0) == "Longitude"){
+                    ui->longitudeField->setText((QString) splitLine.at(3));
+            }else if(splitLine.at(0) == "Latitude"){
+                    ui->LatitudeField->setText((QString) splitLine.at(3));
+            }else if(splitLine.at(0) == "Center"){
+                    mapView->setCenter(QGeoCoordinate(splitLine.at(5).toDouble(),splitLine.at(4).toDouble(),splitLine.at(6).toDouble()));
+            }else if(splitLine.at(0) == "Bottom"){
+
+            }else if(splitLine.at(0) == "Top"){
+
+            }
        }
        file.close();
+    } else{
+        ui->PrecisionValueLabel->setText("0%");
+        ui->precisionSlider->setValue(0);
+        ui->searchStrategyComboBox->setCurrentIndex(0);
+        ui->locateField->setText("0");
+        ui->longitudeField->setText("0");
+        ui->LatitudeField->setText("0");
+        mapView->setCenter(QGeoCoordinate(55.75 , 37.33));
     }
 }
 
@@ -129,13 +147,25 @@ void ConfigWidget::startButtonPush(){
     //    }
     //}
 
+    if(ui->locateField->text().size() != 0){
+        outfile << "Location search field: " << ui->locateField->text().toStdString() << std::endl;
+    }
+
+    if(ui->LatitudeField->text().size() != 0){
+        outfile << "Latitude search field: " << ui->LatitudeField->text().toStdString() << std::endl;
+    }
+
+    if(ui->longitudeField->text().size() != 0){
+        outfile << "Longitude search field: " << ui->longitudeField->text().toStdString() << std::endl;
+    }
+
+    outfile << "Center of the map: " << mapView->center().longitude() << " " << mapView->center().latitude() << " " << mapView->center().altitude() << std::endl;
+
     if(mapView->region().bottomLeft().longitude() != NULL)
     {
-        outfile << "Selected region coorindates: " << std::endl;
-        outfile << "Bottom left: " << mapView->region().bottomLeft().toString().toStdString() << std::endl;
-        outfile << "Bottom right: " << mapView->region().bottomRight().toString().toStdString() << std::endl;
-        outfile << "Top left: " << mapView->region().topLeft().toString().toStdString() << std::endl;
-        outfile << "Top right: " << mapView->region().topRight().toString().toStdString() << std::endl;
+        outfile << "Selected region coorindates in longitude and latitude: " << std::endl;
+        outfile << "Bottom right: " << mapView->region().bottomRight().longitude() << " "  << mapView->region().bottomRight().latitude() << std::endl;
+        outfile << "Top left: " << mapView->region().topLeft().longitude() << " "  << mapView->region().topLeft().latitude() << std::endl;
     }
 
     outfile.close();
