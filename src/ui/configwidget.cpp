995dfbaf15e15d118d3562b6133e9ac3b5b9183a
nemphis <jonas@spanoghe.net>
2016-03-06 13:10:12 +0100

    follow Qt code style

--- src/ui/configwidget.cpp
+++ src/ui/configwidget.cpp
@@ -55,7 +55,7 @@ void ConfigWidget::onMapLoaded()
 
 void ConfigWidget::keyPressEvent(QKeyEvent *event)
 {
-    if(event->key() == Qt::Key_Shift) {
+    if (event->key() == Qt::Key_Shift) {
         if(mapView != NULL)
             mapView->shiftKeyPressed(true);
     }
@@ -64,7 +64,7 @@ void ConfigWidget::keyPressEvent(QKeyEvent *event)
 
 void ConfigWidget::keyReleaseEvent(QKeyEvent *event)
 {
-    if(event->key() == Qt::Key_Shift) {
+    if (event->key() == Qt::Key_Shift) {
         if(mapView != NULL)
             mapView->shiftKeyPressed(false);
     }
@@ -75,32 +75,32 @@ void ConfigWidget::initConfScreen(QString f){
     QFile file(f);
     if (file.open(QIODevice::ReadOnly))
     {
-       QTextStream in(&file);
-       while (!in.atEnd())
-       {
-          QString line = in.readLine();
-          QStringList splitLine = line.split(" ");
-          if(splitLine.at(0) == "Precision:"){
-                    ui->PrecisionValueLabel->setText(splitLine.at(1));
-                    ui->precisionSlider->setValue((((QString)splitLine.at(0).split("%").at(0)).toInt()));
-            } else if(splitLine.at(0) == "Search"){
-                    ui->searchStrategyComboBox->setCurrentText((QString) splitLine.at(2));
-            }else if(splitLine.at(0) == "Location"){
-                    ui->locateField->setText((QString) splitLine.at(3));
-            }else if (splitLine.at(0) == "Longitude"){
-                    ui->longitudeField->setText((QString) splitLine.at(3));
-            }else if(splitLine.at(0) == "Latitude"){
-                    ui->LatitudeField->setText((QString) splitLine.at(3));
-            }else if(splitLine.at(0) == "Center"){
-                    mapView->setCenter(QGeoCoordinate(splitLine.at(5).toDouble(),splitLine.at(4).toDouble(),splitLine.at(6).toDouble()));
-            }else if(splitLine.at(0) == "Bottom"){
+        QTextStream in(&file);
+        while (!in.atEnd())
+        {
+            QString line = in.readLine();
+            QStringList splitLine = line.split(" ");
+            if (splitLine.at(0) == "Precision:") {
+                ui->PrecisionValueLabel->setText(splitLine.at(1));
+                ui->precisionSlider->setValue((((QString)splitLine.at(0).split("%").at(0)).toInt()));
+            } else if (splitLine.at(0) == "Search") {
+                ui->searchStrategyComboBox->setCurrentText((QString) splitLine.at(2));
+            } else if (splitLine.at(0) == "Location") {
+                ui->locateField->setText((QString) splitLine.at(3));
+            } else if (splitLine.at(0) == "Longitude") {
+                ui->longitudeField->setText((QString) splitLine.at(3));
+            } else if (splitLine.at(0) == "Latitude") {
+                ui->LatitudeField->setText((QString) splitLine.at(3));
+            } else if (splitLine.at(0) == "Center") {
+                mapView->setCenter(QGeoCoordinate(splitLine.at(5).toDouble(),splitLine.at(4).toDouble(),splitLine.at(6).toDouble()));
+            } else if (splitLine.at(0) == "Bottom") {
 
-            }else if(splitLine.at(0) == "Top"){
+            } else if (splitLine.at(0) == "Top") {
 
             }
        }
        file.close();
-    } else{
+    } else {
         ui->PrecisionValueLabel->setText("0%");
         ui->precisionSlider->setValue(0);
         ui->searchStrategyComboBox->setCurrentIndex(0);
@@ -131,8 +131,7 @@ void ConfigWidget::startButtonPush(){
 
     std::ofstream outfile(ss.str().c_str());
 
-    if(ui->searchStrategyComboBox->currentText() != NULL)
-    {
+    if (ui->searchStrategyComboBox->currentText() != NULL) {
         outfile << "Search strategy: " << ui->searchStrategyComboBox->currentText().toStdString() << std::endl;
     }
 
@@ -147,22 +146,21 @@ void ConfigWidget::startButtonPush(){
     //    }
     //}
 
-    if(ui->locateField->text().size() != 0){
+    if(ui->locateField->text().size() != 0) {
         outfile << "Location search field: " << ui->locateField->text().toStdString() << std::endl;
     }
 
-    if(ui->LatitudeField->text().size() != 0){
+    if(ui->LatitudeField->text().size() != 0) {
         outfile << "Latitude search field: " << ui->LatitudeField->text().toStdString() << std::endl;
     }
 
-    if(ui->longitudeField->text().size() != 0){
+    if(ui->longitudeField->text().size() != 0) {
         outfile << "Longitude search field: " << ui->longitudeField->text().toStdString() << std::endl;
     }
 
     outfile << "Center of the map: " << mapView->center().longitude() << " " << mapView->center().latitude() << " " << mapView->center().altitude() << std::endl;
 
-    if(mapView->region().bottomLeft().longitude() != NULL)
-    {
+    if(mapView->region().bottomLeft().longitude() != NULL) {
         outfile << "Selected region coorindates in longitude and latitude: " << std::endl;
         outfile << "Bottom right: " << mapView->region().bottomRight().longitude() << " "  << mapView->region().bottomRight().latitude() << std::endl;
         outfile << "Top left: " << mapView->region().topLeft().longitude() << " "  << mapView->region().topLeft().latitude() << std::endl;
