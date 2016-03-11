43b05094680e8ab5bc7533e77c72638e19abff5f
Timon <timon.evenblij@ugent.be>
2016-03-11 20:36:17 +0100

    added the methods for sending status messages and settings messages

--- src/core/drone.cpp
+++ src/core/drone.cpp
@@ -29,6 +29,9 @@ Drone::~Drone()
     delete communicationLink;
 }
 
+/***********************
+Control message methods
+************************/
 
 void Drone::startFlight()
 {
@@ -92,11 +95,121 @@ void Drone::sendWaypoints()
             coordinates.append(coordinate);
     }
     json["Waypoints"] = coordinates;
-
-
-
     QJsonDocument jsondoc(json);
 
     //send the json message
+    communicationLink->doRequest(jsondoc.toJson(QJsonDocument::Indented));
+}
+
+/**************************
+Status messages method
+**************************/
+void Drone::requestStatus(DroneStatus status)
+{
+    std::list<DroneStatus> list = std::list<DroneStatus>();
+    list.push_back(status);
+    requestStatuses(list);
+
+}
+
+void Drone::requestStatuses(std::list<DroneStatus> statuses)
+{
+    //Create json message
+    QJsonObject json = QJsonObject();
+    json["MessageType"]= "status";
+    QJsonArray requestedStatuses = QJsonArray();
+    foreach (const DroneStatus status, statuses){
+        QJsonObject requestedStatus = QJsonObject();
+        QString key;
+        switch (status) {
+        case Battery_Level:
+            key = "battery_level";
+            break;
+        case Location:
+            key = "current_location";
+            break;
+        case Drone_Type:
+            key = "drone_type";
+            break;
+        case Waypoint_Reached:
+            key = "waypoint_reached";
+            break;
+        default:
+            key = "current_lcoation";
+            break;
+        }
+        requestedStatus["Key"] = key;
+        requestedStatuses.append(requestedStatus);
+    }
+    json["Message"] = requestedStatuses;
+    QJsonDocument jsondoc(json);
+
+    //send
+    communicationLink->doRequest(jsondoc.toJson(QJsonDocument::Indented));
+
+}
+
+void Drone::requestHeartbeat()
+{
+    //Create json message
+    QJsonObject json = QJsonObject();
+    json["MessageType"] = "status";
+    json["Message"] = "heartbeat";
+    QJsonDocument jsondoc(json);
+
+    //send
+    communicationLink->doRequest(jsondoc.toJson(QJsonDocument::Indented));
+}
+
+
+/**************************
+Setting messages methods
+**************************/
+
+void Drone::setSetting(DroneSetting setting, int value)
+{
+    std::list<std::pair<DroneSetting, int>> list = std::list<std::pair<DroneSetting, int>>();
+    list.push_back(std::pair<DroneSetting, int>(setting, value));
+    requestStatuses(list);
+}
+
+void Drone::setSettings(std::list<std::pair<DroneSetting, int>> settings)
+{
+    //Create json message
+    QJsonObject json = QJsonObject();
+    json["MessageType"]= "settings";
+    QJsonArray settingsToSet = QJsonArray();
+    int i =0;
+    foreach (const std::pair<DroneStatus, int> setting, settings){
+        QJsonObject settingToSet = QJsonObject();
+        QString key;
+        switch (setting.first) {
+        case Height:
+            key = "height";
+            break;
+        case Speed:
+            key = "speed";
+            break;
+        case Camera_Angle:
+            key = "camera_angle";
+            break;
+        case FPS:
+            key = "fps";
+            break;
+        case Resolution:
+            key = "resolution";
+            break;
+        default:
+            key = "fps";
+            break;
+        }
+        settingToSet["Key"] = key;
+        settingToSet["Value"] = setting.second;
+        settingsToSet.append(settingToSet);
+    }
+    json["Message"] = settingsToSet;
+    QJsonDocument jsondoc(json);
+
+    //send
     communicationLink->doRequest(jsondoc.toJson(QJsonDocument::Indented));
 }
--- src/core/drone.h
+++ src/core/drone.h
@@ -6,6 +6,16 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <QJsonArray>
+#include <QString>
+
+enum DroneStatus
+{
+    Battery_Level, Location, Drone_Type, Waypoint_Reached
+};
+enum DroneSetting
+{
+    Height, Speed, Camera_Angle, FPS, Resolution
+};
 
 /*! \brief This is the class that represents a drone on the workstation.
  * This class can be used to send messages to the drone.
@@ -19,7 +29,7 @@ public:
     **********/
     QString ID; //!< The ID of the drone.
     double visionWidth; //!< This attribute tells how wide the vision of the drone is. This is useful to calculate the waypoints. It should use the same scale as the coordinates used in the waypointplanning algorithms.
-    std::list<QGeoCoordinate> waypoints; //!< Keeps the list of waypoints.
+    std::list<QGeoCoordinate> waypoints; //!< Keeps the list of waypoints the drone needs to fly.
     Communication* communicationLink;
 
     /***********************
@@ -48,6 +58,33 @@ public:
 
     //! Sends a Json message to the drone to make an emergency landing.
     void emergencyLanding();
+
+    /**************************
+    Status messages method
+    **************************/
+    /*! \brief Sends a Json message to the drone to request a certain status.
+     *  See DroneStatus enum to see which statuses can be requested.
+     * This method actually uses the method to request multiple statuses, namely requestStatuses(). */
+    void requestStatus(DroneStatus status);
+    /*! \brief Sends a Json message to the drone to request certain multiple statuses.
+     *  See DroneStatus enum to see which statuses can be requested. */
+    void requestStatuses(std::list<DroneStatus> statuses);
+    //! Sends a Json message that asks for the heartbeat.
+    void requestHeartbeat();
+
+    /**************************
+    Setting messages methods
+    **************************/
+    /*! \brief Sends a Json message to set a certain setting to a certain value.
+     *  See DroneStatus enum to see which settings can be set.
+     * This method actually uses the method to set mulitple settings, namely setSettings().
+    */
+    void setSetting(DroneSetting setting, int value);
+
+    /*! \brief Sends a Json message to set certain settings to a certain values.
+     * See Dronestatus enum to see which settings can be set.
+    */
+    void setSettings(std::list<DroneSetting> settings, std::List<int> values);
 
 
 
