31a4a7154832fecc7e0596f7972b4b31d803220a
Jonas <jonasleo.spanoghe@ugent.be>
2016-03-20 10:29:28 +0100

    fix some issues in theme

--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile file(":styles/main.qss");
-    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        a.setStyleSheet(file.readAll());
-        file.close();
-    }
-
     qDebug() << "Starting";
-
 
     // Drone drone =Drone(0.000005);
     QUuid droneId = QUuid::createUuid();
--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -20,28 +20,12 @@ MainWindow::MainWindow(QWidget *parent) :
 
     statusBar()->addWidget(new QLabel(tr("Ready")));
 
-//    qApp->setStyle(QStyleFactory::create("Fusion"));
     qApp->setStyle("Fusion");
-
-    QPalette darkPalette;
-    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
-    darkPalette.setColor(QPalette::WindowText, Qt::white);
-    darkPalette.setColor(QPalette::Base, QColor(25,25,25));
-    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
-    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
-    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
-    darkPalette.setColor(QPalette::Text, Qt::white);
-    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
-    darkPalette.setColor(QPalette::ButtonText, Qt::white);
-    darkPalette.setColor(QPalette::BrightText, Qt::red);
-    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-
-    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
-
-//    qApp->setPalette(darkPalette);
-
-//    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+    QFile file(":styles/main.qss");
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qApp->setStyleSheet(file.readAll());
+        file.close();
+    }
 
     connect(ui->exitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
     connect(ui->connectAction, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
