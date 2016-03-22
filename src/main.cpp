cfc23562fe416ba27db0c89510dd33243052e99a
vdutordo <vincent.dutordoir@ugent.be>
2016-03-22 13:07:52 +0100

    deleted comment in main

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-//    QFile file(":styles/main.qss");
-//    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-//        a.setStyleSheet(file.readAll());
-//        file.close();
-//    }
-
     // testDrone();
 
     MainWindow w;
