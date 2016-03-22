6f1a7d78f4124d321f0b3479417a774fa3d69beb
jonas <jonasleo.spanoghe@ugent.be>
2016-03-22 13:06:10 +0100

    change test-server

--- tests/server.py
+++ tests/server.py
@@ -19,23 +19,11 @@ while True:
         buffersize = struct.unpack(">I", length)[0]
         # print buffersize
     data = client.recv(buffersize)
-    # print "received message:{0}".format(data)
-    packet = json.loads(data)  # parse the Json we received
+    print "received message:{0}".format(data)
 
-    if 'MessageType' not in packet:  # every packet should have a MessageType field
-        raise ValueError
-    if 'Message' not in packet:  # every packet should have a Message field
-        raise ValueError
-
-    message_type = packet['MessageType']  # the 'message type' attribute tells us to which class of packet this packet belongs
-    message = packet['Message']           # the 'message' attribute tells what packet it is, within it's class
-    print message
-
-    # response = "Roger that golden eagle"
-    start_message = {'MessageType': 'status', 'Speed': 10}
-    response = json.dumps(start_message)
+    response = "Roger that golden eagle"
     print "responding"
-    # client.send(format(len(response), '#04x') + response)
+    client.send(format(len(response), '#04x') + response)
     preamble = bytearray(struct.pack(">H", len(response) + 4))
     preamble += bytearray(struct.pack(">I", len(response)))
     client.send(preamble + response)
