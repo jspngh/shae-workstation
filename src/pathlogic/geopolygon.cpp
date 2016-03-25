b9d95e4617246fce1eaf43dfc7b05ada6d863cf6
Timon <timon.evenblij@ugent.be>
2016-03-25 13:04:06 +0100

    more implementations of basic stuff

---
+++ src/pathlogic/geopolygon.cpp
@@ -0,0 +1,109 @@
+#include "geopolygon.h"
+#include <algorithm>
+
+GeoPolygon::GeoPolygon()
+{
+
+}
+
+GeoPolygon::GeoPolygon(QList<QGeoCoordinate> coordinates)
+{
+    //TODO: set mostWestCoordinate and set mostEastCoordinate
+    /*
+    double minLongitude = 180;
+    double maxLongitude = -180;
+
+    foreach(QGeoCoordinate coordinate, coordinates){
+        if(coordinate.longitude() > maxLongitude )
+            maxLongitude = coordinate.longitude();
+            mostEastCoordinate = coordinate;
+        if(coordinate.longitude() < minLongitude)
+            minLongitude = coordinate.longitude();
+            mostWestCoordinate = coordinate;
+    }
+    */
+    std::sort(coordinates.begin(), coordinates.end(), compare);
+    mostWestCoordinate = coordinates.front();
+    mostEastCoordinate = coordinates.back();
+
+
+
+    //TODO: create upperhull and lowerhull
+
+
+    //if not valid, just return default constructor
+    if(!isValid()){
+        qDebug()<< "The polygon area created is not valid, using the default constructor now instead";
+        return GeoPolygon();
+    }
+}
+
+int compare(const QGeoCoordinate left, const QGeoCoordinate right)
+{
+    if(left.longitude() == right.longitude())
+        return left.latitude() < right.latitude();
+    else return left.longitude() < right.longitude();
+}
+
+double GeoPolygon::crossProduct(QGeoCoordinate O, QGeoCoordinate A, QGeoCoordinate B)
+{
+    return (A.longitude() - O.longitude())
+            * (B.latitude() - O.latitude())
+            - (A.latitude() - O.latitude())
+            * (B.longitude() - O.longitude());
+}
+
+
+
+bool GeoPolygon::isValid() const
+{
+    //check if mostEast en mostWest are correct
+    foreach(QGeoCoordinate coordinate, coordinates){
+        if (coordinate.longitude() < mostWestCoordinate)
+            return false;
+        if(coordinate.longitude() > mostEastCoordinate)
+            return false;
+    }
+
+
+    //mostleft and most right should be in both hulls at resp. front and back
+    if(upperHull.front() != mostWestCoordinate || lowerHull.front() != mostWestCoordinate)
+       return false;
+    if(upperHull.back() != mostWestCoordinate || lowerHull.back() != mostWestCoordinate)
+       return false;
+
+    //TODO: check if upperhull curves clockwise
+
+    //TODO: check if lowerhull curves counter-clockwise
+
+    //TODO: check if lowerhull is completely below upperhull.
+
+
+
+    return true;
+}
+
+QList<QGeoCoordinate> GeoPolygon::getCoordinates() const
+{
+    return coordinates;
+}
+
+QList<QGeoCoordinate> GeoPolygon::getUpperHull() const
+{
+    return upperHull;
+}
+
+QList<QGeoCoordinate> GeoPolygon::getLowerHull() const
+{
+    return lowerHull;
+}
+
+QGeoCoordinate GeoPolygon::getMostWestCoordinate() const
+{
+    return mostWestCoordinate;
+}
+
+QGeoCoordinate GeoPolygon::getMostEastCoordinate() const
+{
+    return mostEastCoordinate;
+}
---
+++ src/pathlogic/geopolygon.h
@@ -0,0 +1,53 @@
+#ifndef GEOPOLYGON_H
+#define GEOPOLYGON_H
+#include <QGeoShape>
+#include <QGeoRectangle>
+
+/*!
+ * \brief The GeoPolygon class describes a convex polygon area.
+ * It is derived from QGeoShape, the base class of QGeoRectangle.
+ */
+class GeoPolygon : public QGeoShape
+{
+public:
+    GeoPolygon();
+    GeoPolygon(QList<QGeoCoordinate> coordinates);
+
+    QGeoRectangle getBoundingQGeoRectangle();
+
+    /*! \brief This functions calculates the z-component of the cross product to know if the points
+        make a clock-wise turn or not, useful for checking if a hull is valid.
+        \return returns positive if OAB makes a counter-clockwise turn,
+        negative if clockwise and zero if points are collinear. */
+    double crossProduct(QGeoCoordinate O, QGeoCoordinate A, QGeoCoordinate B);
+    /***********
+     * Getters *
+     ***********/
+    QList<QGeoCoordinate> getCoordinates() const;
+
+    QList<QGeoCoordinate> getUpperHull() const;
+
+    QList<QGeoCoordinate> getLowerHull() const;
+
+    QGeoCoordinate getMostWestCoordinate() const;
+
+    QGeoCoordinate getMostEastCoordinate() const;
+
+private:
+
+    //! Checks if the list of coordinates forms a valid convex polygon.
+    bool isValid() const;
+
+    //! The list of coordinates contained by the polygon.
+    QList<QGeoCoordinate> coordinates;
+
+    //The following attributes are only for internal caching use, these should never be set.
+    //! contains all coordinates from the upper hull, sorted from mostWest to mostEast.
+    QList<QGeoCoordinate> upperHull;
+    //! contains all coordinates from the lower hull, sorted from mostWest to mostEast;
+    QList<QGeoCoordinate> lowerHull;
+    QGeoCoordinate mostWestCoordinate;
+    QGeoCoordinate mostEastCoordinate;
+};
+
+#endif // GEOPOLYGON_H
