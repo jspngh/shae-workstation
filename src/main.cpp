6183600d15eeb94390094428a8822eff5aa81d8f
lhulstae <lars.hulstaert@ugent.be>
2016-03-31 13:23:55 +0200

    entered main

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,13 @@
 
 int main(int argc, char *argv[])
 {
-   QList<Drone*> *droneList; QGeoRectangle area;
-   Search* s = new Search();
-   s->setHeight(3);
-   s->setGimbalAngle(65);
-   Mediator* m;
-   // "dependencies/drone_stream.mpg"
-   QString footage = "dependencies/testfootage.mp4";
-   cv::VideoCapture capture = cv::VideoCapture(footage.toStdString());
-   DetectionController* d = new DetectionController(m, s, 2, capture);
-   d->start();
-   d->wait();
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.show();
+
+    Controller controller(&w);
+    controller.init();
+
+    return a.exec();
 }
