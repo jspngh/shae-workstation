b9847a66398459c6ab3e7c6e31ee8237a89decd9
vpolflie <vincent.polfliet@ugent.be>
2016-04-01 16:25:29 +0200

    generaltest added fixing persistence

---
+++ tests/unit_tests/core/generaldao_test.cpp
@@ -0,0 +1,43 @@
+#include <QtTest/QtTest>
+#include <QString>
+#include "generaldao_test.h"
+#include "persistence/generaldao.h"
+
+GeneralDAO_Test::GeneralDAO_Test()
+{
+
+}
+
+GeneralDAO_Test::~GeneralDAO_Test()
+{
+}
+
+void GeneralDAO_Test::initTestCase()
+{
+}
+
+void GeneralDAO_Test::cleanupTestCase()
+{
+}
+
+void GeneralDAO_Test::testUncypherGeneralDAO()
+{
+    QString testString = QString("5-5:6-6:7-7:8-8");
+    GeneralDAO generaldao = GeneralDAO();
+    QList<QGeoCoordinate> returnList = generaldao.uncypherPathString(testString);
+    QVERIFY(retrunList.size() == 4);
+    double i = 5;
+    for(QGeoCoordinate coordinate: returnList)
+    {
+        QVERIFY(coordinate.longitude() == i);
+        QVERIFY(coordinate.latitude() == i);
+        i++;
+    }
+}
+
+void GeneralDAO_Test::testUncypherEmptyStringGeneralDAO()
+{
+    QString testString = QString("5-5:6-6:7-7:8-8");
+    GeneralDAO generaldao = GeneralDAO();
+    QVERIFY(generaldao.uncypherPathString(testString).empty());
+}
