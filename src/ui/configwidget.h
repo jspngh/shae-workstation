627362fc31fe9ea5bddfc47f8a61adb2711afe5f
Timon <timon.evenblij@ugent.be>
2016-05-05 14:19:21 +0200

    Added docs in ui, not finished

--- src/ui/configwidget.h
+++ src/ui/configwidget.h
@@ -21,35 +21,50 @@ class ConfigWidget;
 }
 
 /*!
- * \brief The ConfigWidget class
+ * \brief The ConfigWidget class represents the code for the configuration screen.
  * \ingroup Ui
+ * The layout is defined in configwidget.ui
  */
 class ConfigWidget : public QWidget
 {
     Q_OBJECT
 
 public:
+    //! constructor
     explicit ConfigWidget(QWidget *parent = 0);
+    //! destructor
     ~ConfigWidget();
+    //! This is called when a key is pressed
     void keyPressEvent(QKeyEvent *event);
+    //! This is called when a key is released
     void keyReleaseEvent(QKeyEvent *event);
+    //! Sets the mediator to allow connecting signals and slots without direct dependencies
     void setMediator(Mediator *mediator);
 
 signals:
+    //! This signal is emitted when a search is to start, i.e. when the start search buttong is clicked.
     void startSearch(Search *s);
+    //! This signal is emitted to ask for the status of the connected drones.
     void requestDronesStatus();
 
 public slots:
+    //! When the mapview is loaded, this slot is called. it makes the mapview visible.
     void onMapLoaded();
+    //! When the mapview failed to load, this slot is called.
     void onMapFailedToLoad();
 
 private slots:
+    //! Called when the start button is pushed, this will check if everything is correct and whill emit startSearch(s)
     void startButtonPush();
+    //! Called when the locate button is pushed, this will center the map on the specified location
     void locateButtonPush();
+    //! Deprecated: Not used, does not do anything
     void sliderChanged(int);
+    //! When an area is selected, this slot is called. It will set the boolean areaWasSelected to true.
     void areaSelected(QGeoRectangle area);
-    //! \brief slot will listen to incoming DroneStatuses and then update the dronetable
+    //! a slot that will listen to incoming DroneStatuses and then update the dronetable
     void updateDroneTable(DroneStatus* s);
+    //! updates the map center
     void updateMapCenter(DroneStatus* s);
 
 private:
