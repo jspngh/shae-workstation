25ff452a5f8e8c1690488357c5bdd3912ba5e6aa
vdutordo <vincent.dutordoir@ugent.be>
2016-05-17 14:31:38 +0200

    added resetscriptcontroller

---
+++ src/ui/controllers/resetscriptcontroller.h
@@ -0,0 +1,19 @@
+#ifndef RESETSCRIPTCONTROLLER_H
+#define RESETSCRIPTCONTROLLER_H
+
+#include <QObject>
+
+class ResetScriptController: public QObject
+{
+    Q_OBJECT
+public:
+    ResetScriptController(QObject* parent = 0);
+
+signals:
+    void resetSuccessful();
+
+public slots:
+    void reset();
+};
+
+#endif // RESETSCRIPTCONTROLLER_H
